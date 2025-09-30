beab76878037e9731899ed44a8b58d79ece8f8f7
NesmaElsafty <nesmaelsaftysm@gmail.com>
2025-09-30 16:26:19 +0300

    first commit

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('national_id')->unique();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->text('summary')->nullable();
+            $table->string('password')->nullable();
+            $table->enum('type', ['user', 'admin', 'agent'])->default('user');
+            $table->boolean('is_active')->default(true);
+            // softdelete
+            $table->softDeletes();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        // generate saudi data for user
+        User::factory()->create([
+            'name' => 'Saudi User',
+            'national_id' => '12345678901234',
+            'phone' => '+966500000000',
+            'address' => 'Riyadh, SA',
+            'summary' => 'I am a Saudi user',
+            'type' => 'user',
+            'is_active' => true,
+            'password' => Hash::make('123456'),
+        ]);
+
+        // generate saudi data for agent
+        User::factory()->create([
+            'name' => 'Saudi Agent',
+            'national_id' => '12345678901235',
+            'phone' => '+966500000001',
+            'address' => 'Riyadh, SA',
+            'summary' => 'I am a Saudi agent',
+            'type' => 'agent',
+            'is_active' => true,
+            'password' => Hash::make('123456'),
+        ]);
+
+        // generate saudi data for admin
+        User::factory()->create([
+            'name' => 'Saudi Admin',
+            'national_id' => '12345678901236',
+            'phone' => '+966500000002',
+            'address' => 'Riyadh, SA',
+            'summary' => 'I am a Saudi admin',
+            'type' => 'admin',
+            'is_active' => true,
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
